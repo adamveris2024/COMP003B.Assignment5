c4bbbcc39a80f96bf12cfc4a1829063cc42a8162
adamveris2024 <adamveris2024@gmail.com>
2025-04-25 15:35:51 -0700

    finished BoardGameController

---
+++ Controllers/BoardGameController.cs
@@ -0,0 +1,66 @@
+﻿using COMP003B.Assignment5.Data;
+using COMP003B.Assignment5.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace COMP003B.Assignment5.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BoardGameController : Controller
+    {
+        [HttpGet]
+        public ActionResult<List<BoardGame>> GetBoardGames()
+        {
+            return Ok(BoardGameStore.BoardGames);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<BoardGame> GetBoardGame(int id)
+        {
+            var boardgame = BoardGameStore.BoardGames.FirstOrDefault(p => p.Id == id);
+            
+            if (boardgame is null)
+                return NotFound();
+
+            return Ok(boardgame);
+        }
+
+        [HttpPost]
+        public ActionResult<BoardGame> CreateBoardGame(BoardGame boardgame)
+        {
+            boardgame.Id = BoardGameStore.BoardGames.Max(p => p.Id) + 1;
+
+            BoardGameStore.BoardGames.Add(boardgame);
+
+            return CreatedAtAction(nameof(GetBoardGame), new { id = boardgame.Id }, boardgame);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateBoardGame(int id, BoardGame updatedBoardGame)
+        {
+            var existingBoardGame = BoardGameStore.BoardGames.FirstOrDefault(p => p.Id == id);
+
+            if (existingBoardGame is null)
+                return NotFound();
+
+            existingBoardGame.Name = updatedBoardGame.Name;
+            existingBoardGame.MaxPlayers = updatedBoardGame.MaxPlayers;
+            existingBoardGame.MinPlayers = updatedBoardGame.MinPlayers;
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBoardGame(int id)
+        {
+            var boardgame = BoardGameStore.BoardGames.FirstOrDefault(p => p.Id == id);
+
+            if (boardgame is null)
+                return NotFound();
+
+            BoardGameStore.BoardGames.Remove(boardgame);
+
+            return NoContent();
+        }
+    }
+}
--- Data/BoardGameStore.cs
+++ Data/BoardGameStore.cs
@@ -1,6 +1,14 @@
-﻿namespace COMP003B.Assignment5.Data
+﻿using COMP003B.Assignment5.Models;
+
+namespace COMP003B.Assignment5.Data
 {
     public class BoardGameStore
     {
+        public static List<BoardGame> BoardGames { get; } = new()
+        {
+            new BoardGame { Id = 1, Name = "Monopoly", MinPlayers = 2, MaxPlayers = 8},
+            new BoardGame { Id = 2, Name = "Uno", MinPlayers = 2, MaxPlayers = 4},
+            new BoardGame { Id = 3, Name = "Jenga", MinPlayers = 2, MaxPlayers = 8}
+        };
     }
 }
